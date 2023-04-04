829b972d35c8ef890e36e9464f66a53563214e0e
Breanna Roncal <36829789+BreannaRoncal@users.noreply.github.com>
2023-04-03 23:31:08 -0700

    Update iterative_fibonacci.py

--- iterative_fibonacci.py
+++ iterative_fibonacci.py
@@ -10,7 +10,7 @@ def iterative_fibonacci(num):
 
 
 # test cases
-print(fibonacci(3) == 2)
-print(fibonacci(7) == 13)
-print(fibonacci(0) == 0)
-print(fibonacci(-1) == -1)
+print(iterative_fibonacci(3) == 2)
+print(iterative_fibonacci(7) == 13)
+print(iterative_fibonacci(0) == 0)
+print(iterative_fibonacci(-1) == -1)
