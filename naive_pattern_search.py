6f9fb388f930c71050095ffdbcea11cfec44a729
Breanna Roncal <36829789+BreannaRoncal@users.noreply.github.com>
2023-04-10 16:55:11 -0700

    Update naive_pattern_search.py

--- naive_pattern_search.py
+++ naive_pattern_search.py
@@ -7,6 +7,26 @@ This will be done through the following steps:
   1. Find patterns more easily by making the search case-insensitive.
   2. Build and maintain a separate copy of the introduction to insert replacements.
   3. Skip newly-replaced characters.
+  
+  
+Replace Found Words:
+
+  1. Maintaining the Fixed Text
+      a) Add an additional replacement input parameter to the function definition for the text that will replace the found pattern.
+      b) Initialize a fixed_text variable at the top of the function that will serve as a placeholder for the completed text containing 
+         all the necessary replacements to be returned at the end of the function.
+      c) When the pattern is found, the replacement should now be appended to the fixed_text. In all other cases, append the currently 
+         iterating text character to the fixed_text to ensure that the rest of the original text is being maintained.
+         
+         
+  2. Skipping Replaced Characters
+      a) Initialize a num_skips variable set to 0 right after the initialization of the fixed_text variable. 
+         This will track the numbers of characters that need to be skipped during the search, as a pattern was already found, and the relevant 
+         characters in the text already replaced.
+      b) While iterating through the text indices, if num_skips is greater than 0, decrement it by 1 and continue to the next iteration of the for loop. 
+         This logic allows for the actual skipping of the replaced characters.
+      c) Right after the replacement is appended to the fixed_text, set num_skips to the length of the pattern minus 1. 
+         This sets the number of following search iterations to skip. 1 must be subtracted to account for the current iteration.
 """
 
 def pattern_search(text, pattern, replacement, case_sensitive=True):
